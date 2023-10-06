ef21aa02bfa4b2f1e16a49670430b98acae8f20d
Irulik <123405063+Irulik@users.noreply.github.com>
2023-10-06 20:02:04 +0200

    Update 1.ts

--- src/1.ts
+++ src/1.ts
@@ -5,9 +5,10 @@
 */
 
 class Student {
-  constructor(public name: string, public age: number, public grade: string) {}
+  constructor(public name: string, public age: number, public grade: string) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
 }
-
-const student = new Student("Anna", 20, "A");
 
 export {};
