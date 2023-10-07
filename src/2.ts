27b44ed123af8890038e2c9797db76d6c84391ff
Irulik <123405063+Irulik@users.noreply.github.com>
2023-10-07 10:40:01 +0200

    Update 2.ts

--- src/2.ts
+++ src/2.ts
@@ -30,9 +30,8 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
   constructor(name: string, department: string, salary: number) {
-    super(name, "Manager", salary + 10000);
+    super(name, department, salary + 10000);
   }
 }
 
